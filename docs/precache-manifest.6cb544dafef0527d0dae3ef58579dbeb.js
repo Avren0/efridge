79b8bb1d3954d6ca3851a1c7077d5156e9500650
Krzysztof Pieprzyca <30498780+Avren0@users.noreply.github.com>
2019-06-05 18:21:38 +0200

    deploy

---
+++ docs/precache-manifest.6cb544dafef0527d0dae3ef58579dbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cad8da767a46bdd27f63cdadbad7b3d",
+    "url": "/efridge/index.html"
+  },
+  {
+    "revision": "36118bbb92bbcd6762d2",
+    "url": "/efridge/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e79e4e66ce884e24d9ec",
+    "url": "/efridge/static/css/main.95832410.chunk.css"
+  },
+  {
+    "revision": "36118bbb92bbcd6762d2",
+    "url": "/efridge/static/js/2.13694f8b.chunk.js"
+  },
+  {
+    "revision": "e79e4e66ce884e24d9ec",
+    "url": "/efridge/static/js/main.44ab2dd1.chunk.js"
+  },
+  {
+    "revision": "df4008d4f8af7ca12062",
+    "url": "/efridge/static/js/runtime~main.e4af0ce6.js"
+  }
+]);
